e11c947aaa72e1427ed24ad5b4354dfcd4c9e7ed
schroeji <schroeji@studi.informatik.uni-stuttgart.de>
2021-01-31 18:02:47 +0100

    Find map fix

--- src/misc/manager.cpp
+++ src/misc/manager.cpp
@@ -2,52 +2,53 @@
 #include "memory_access.hpp"
 #include "util.hpp"
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <stdlib.h>
 #include <chrono>
-#include <thread>
+#include <fstream>
+#include <iostream>
 #include <stdexcept>
+#include <stdlib.h>
+#include <string>
+#include <thread>
 
 using namespace std;
 
-GameManager::GameManager(MemoryAccess& mem) : mem(mem),
-                                             settings(Settings::getInstance()) {
+GameManager::GameManager(MemoryAccess &mem)
+    : mem(mem), settings(Settings::getInstance()) {
   bool fresh_launch = false;
-  while (!mem.getPid()) {  // wait until game has launched
+  while (!mem.getPid()) { // wait until game has launched
     if (!fresh_launch)
       cout << "Waiting for game to launch..." << endl;
     fresh_launch = true;
     this_thread::sleep_for(chrono::milliseconds(1000));
   }
-  if(fresh_launch)              // additional wait when game just launched
+  if (fresh_launch) // additional wait when game just launched
     this_thread::sleep_for(chrono::milliseconds(15000));
   if (!mem.getClientRange().first) {
     cout << "Could not find Client Base" << endl;
     exit(0);
   }
-  if (!mem.getEngineRange().first){
+  if (!mem.getEngineRange().first) {
     cout << "Could not find Engine Base" << endl;
     exit(0);
   }
 }
 
-void GameManager::grabPlayers(){
-	if (!mem.read((void*) mem.glow_addr, &manager, sizeof(GlowObjectManager_t))) {
+void GameManager::grabPlayers() {
+  if (!mem.read((void *)mem.glow_addr, &manager, sizeof(GlowObjectManager_t))) {
     cout << "Could not get GlowObjectManager" << endl;
     return;
   }
-	size_t count = manager.objects.Count;
-	void* data_ptr = (void*) manager.objects.DataPtr;
+  size_t count = manager.objects.Count;
+  void *data_ptr = (void *)manager.objects.DataPtr;
   // cout << hex << "Data : " << data_ptr << endl;
   // if (settings.debug) cout << dec << "object count: " << count << endl;
-  if(!mem.read(data_ptr, (void*) g_glow, sizeof(GlowObjectDefinition_t) * count)){
+  if (!mem.read(data_ptr, (void *)g_glow,
+                sizeof(GlowObjectDefinition_t) * count)) {
     cout << "Could not get objects" << endl;
     return;
   }
-  vector<EntityType*> new_players;
-  vector<EntityType*> new_nonPlayerEntities;
+  vector<EntityType *> new_players;
+  vector<EntityType *> new_nonPlayerEntities;
   vector<addr_type> new_player_addrs;
   local_player_index = -1;
   for (unsigned int i = 0; i < count; i++) {
@@ -55,26 +56,27 @@ void GameManager::grabPlayers(){
     if (g_glow[i].m_pEntity == 0)
       continue;
 
-    EntityType* entity = new EntityType;
+    EntityType *entity = new EntityType;
     mem.read(g_glow[i].m_pEntity, entity, sizeof(EntityType));
-    if ((entity->m_iTeamNum == Team::CT || entity->m_iTeamNum == Team::T) && entity->m_iHealth > 0) {
-      // cout << dec << "player: " << new_players.size() << " addr: " << objects[i].m_pEntity << endl;
+    if ((entity->m_iTeamNum == Team::CT || entity->m_iTeamNum == Team::T) &&
+        entity->m_iHealth > 0) {
+      // cout << dec << "player: " << new_players.size() << " addr: " <<
+      // objects[i].m_pEntity << endl;
       new_players.push_back(entity);
-      new_player_addrs.push_back((addr_type) g_glow[i].m_pEntity);
-      if (g_glow[i].m_pEntity == (void*) mem.local_player_addr)
+      new_player_addrs.push_back((addr_type)g_glow[i].m_pEntity);
+      if (g_glow[i].m_pEntity == (void *)mem.local_player_addr)
         local_player_index = new_players.size() - 1;
-    }
-    else
+    } else
       new_nonPlayerEntities.push_back(entity);
   }
   // copy new players and delete old ones afterwards for thread safety
-  vector<EntityType*> old_players = players;
+  vector<EntityType *> old_players = players;
   players = new_players;
   player_addrs = new_player_addrs;
   nonPlayerEntities = new_nonPlayerEntities;
-  for (EntityType* player : old_players)
+  for (EntityType *player : old_players)
     delete player;
-  for (EntityType* ent : new_nonPlayerEntities)
+  for (EntityType *ent : new_nonPlayerEntities)
     delete ent;
 
   // refresh helper variables
@@ -84,29 +86,34 @@ void GameManager::grabPlayers(){
     local_player_flashed_timer = 0.0;
 }
 
-vector<EntityType*>& GameManager::getPlayers() {
-  return players;
-}
+vector<EntityType *> &GameManager::getPlayers() { return players; }
 
 void GameManager::printPlayers() {
   int i = 0;
   cout << "---------Players---------" << endl;
-  for (EntityType* player : players) {
+  for (EntityType *player : players) {
     cout << dec << "--- Player: " << i << "---" << endl;
     cout << hex << "Addr: " << player_addrs[i] << endl;
     printf("ID: %d\n", player->m_iEntityId);
     cout << dec << "hp: " << player->m_iHealth << endl;
-    if(player->m_iTeamNum == Team::CT)
+    if (player->m_iTeamNum == Team::CT)
       cout << "Team: CT" << endl;
-    else if(player->m_iTeamNum == Team::T)
+    else if (player->m_iTeamNum == Team::T)
       cout << "Team: T" << endl;
-    printf("Origin x=%f y=%f z=%f\n", player->m_vecOrigin.x, player->m_vecOrigin.y, player->m_vecOrigin.z);
-    // printf("Angle: x=%4.16lf y=%4.16lf z=%f\n", player->m_angAbsRotation.x, player->m_angAbsRotation.y, player->m_angNetworkAngles.z);
-    printf("Angle: x=%4.16lf y=%4.16lf z=%f\n", player->m_angNetworkAngles.x, player->m_angNetworkAngles.y, player->m_angNetworkAngles.z);
-    printf("view offset: %f, %f\n", player->m_vecViewOffset.x,  player->m_vecViewOffset.y);
+    printf("Origin x=%f y=%f z=%f\n", player->m_vecOrigin.x,
+           player->m_vecOrigin.y, player->m_vecOrigin.z);
+    // printf("Angle: x=%4.16lf y=%4.16lf z=%f\n", player->m_angAbsRotation.x,
+    // player->m_angAbsRotation.y, player->m_angNetworkAngles.z);
+    printf("Angle: x=%4.16lf y=%4.16lf z=%f\n", player->m_angNetworkAngles.x,
+           player->m_angNetworkAngles.y, player->m_angNetworkAngles.z);
+    printf("view offset: %f, %f\n", player->m_vecViewOffset.x,
+           player->m_vecViewOffset.y);
     printf("m_fFlags: %u\n", player->m_fFlags);
-    printf("Velocity: %f, %f, %f\n", player->m_vecVelocity.x,  player->m_vecVelocity.y, player->m_vecVelocity.z);
-    printf("Aimpunch: %f, %f, %f\n", getAimPunch(mem.local_player_addr).x,  getAimPunch(mem.local_player_addr).y, getAimPunch(mem.local_player_addr).z);
+    printf("Velocity: %f, %f, %f\n", player->m_vecVelocity.x,
+           player->m_vecVelocity.y, player->m_vecVelocity.z);
+    printf("Aimpunch: %f, %f, %f\n", getAimPunch(mem.local_player_addr).x,
+           getAimPunch(mem.local_player_addr).y,
+           getAimPunch(mem.local_player_addr).z);
     printf("Defusing: %d\n", isDefusing(player_addrs[i]));
     printf("Flashed: %d\n", isFlashed(player_addrs[i]));
     printf("Weapon: %s\n", getWeaponName(getWeapon(player_addrs[i])).c_str());
@@ -123,21 +130,22 @@ void GameManager::printPlayers() {
 }
 
 void GameManager::printEntities() {
-  if (! mem.read((void*) mem.glow_addr, &manager, sizeof(GlowObjectManager_t))) {
+  if (!mem.read((void *)mem.glow_addr, &manager, sizeof(GlowObjectManager_t))) {
     cout << "Could not get GlowObjectManager" << endl;
     return;
   }
-	size_t count = manager.objects.Count;
-	void* data_ptr = (void*) manager.objects.DataPtr;
+  size_t count = manager.objects.Count;
+  void *data_ptr = (void *)manager.objects.DataPtr;
   // cout << hex << "Data : " << data_ptr << endl;
   // cout << dec << "count : " << count << endl;
-  if(!mem.read(data_ptr, (void*) g_glow, sizeof(GlowObjectDefinition_t) * count)){
+  if (!mem.read(data_ptr, (void *)g_glow,
+                sizeof(GlowObjectDefinition_t) * count)) {
     cout << "Could not get objects" << endl;
     return;
   }
   cout << "----------Entities------------" << endl;
   for (unsigned int i = 0; i < count; i++) {
-    EntityType* entity = new EntityType;
+    EntityType *entity = new EntityType;
     if (g_glow[i].m_pEntity == nullptr)
       continue;
     cout << dec << "Nr: " << i << endl;
@@ -145,7 +153,8 @@ void GameManager::printEntities() {
     mem.read(g_glow[i].m_pEntity, entity, sizeof(EntityType));
     printf("ID: %d\n", entity->m_iEntityId);
     cout << "hp: " << entity->m_iHealth << endl;
-    printf("Origin x=%f y=%f z=%f\n", entity->m_vecOrigin.x, entity->m_vecOrigin.y, entity->m_vecOrigin.z);
+    printf("Origin x=%f y=%f z=%f\n", entity->m_vecOrigin.x,
+           entity->m_vecOrigin.y, entity->m_vecOrigin.z);
     printf("m_fFlags: %u\n", entity->m_fFlags);
     printf("m_iEFlags: %d\n", entity->m_iEFlags);
     vector<int> diffs = mem.diffMem((addr_type)g_glow[i].m_pEntity, 0x200);
@@ -158,7 +167,7 @@ void GameManager::printEntities() {
   }
 }
 
-void GameManager::printPlayerLocationsToFile(const string& filename) {
+void GameManager::printPlayerLocationsToFile(const string &filename) {
   if (players.empty())
     return;
   ofstream file;
@@ -167,23 +176,23 @@ void GameManager::printPlayerLocationsToFile(const string& filename) {
   file << local_player_index << ",0,0,0,0,0" << endl;
   // Format: number,hp,team,x,y,z
   int i = 0;
-  for (EntityType* player : players) {
+  for (EntityType *player : players) {
     file << i << ",";
     file << player->m_iHealth << ",";
     file << player->m_iTeamNum << ",";
-    file << player->m_vecOrigin.x << "," << player->m_vecOrigin.y << "," << player->m_vecOrigin.z << endl;
+    file << player->m_vecOrigin.x << "," << player->m_vecOrigin.y << ","
+         << player->m_vecOrigin.z << endl;
     i++;
   }
   file.close();
 }
 
-MemoryAccess& GameManager::getMemoryAccess() {
-  return mem;
-}
+MemoryAccess &GameManager::getMemoryAccess() { return mem; }
 
-EntityType* GameManager::getLocalPlayer() {
+EntityType *GameManager::getLocalPlayer() {
   mem.updateLocalPlayerAddr();
-  if (!mem.read((void*) mem.local_player_addr, (void*) local_player, sizeof(EntityType))) {
+  if (!mem.read((void *)mem.local_player_addr, (void *)local_player,
+                sizeof(EntityType))) {
     connected = false;
     throw runtime_error("No local player");
   }
@@ -191,26 +200,22 @@ EntityType* GameManager::getLocalPlayer() {
   return local_player;
 }
 
-int GameManager::getLocalPlayerIndex() {
-  return local_player_index;
-}
+int GameManager::getLocalPlayerIndex() { return local_player_index; }
 
-bool GameManager::gameRunning() {
-  return mem.getPid() > 0;
-}
+bool GameManager::gameRunning() { return mem.getPid() > 0; }
 
 bool GameManager::isOnServer() {
   if (!connected) {
     try {
       getLocalPlayer();
-    } catch(const runtime_error& e) {
+    } catch (const runtime_error &e) {
       return false;
     }
   }
   return connected;
 }
 
-addr_type GameManager::getPlayerAddr(EntityType* player) {
+addr_type GameManager::getPlayerAddr(EntityType *player) {
   for (unsigned int i = 0; i < players.size(); i++) {
     if (player == players[i])
       return player_addrs[i];
@@ -219,50 +224,66 @@ addr_type GameManager::getPlayerAddr(EntityType* player) {
 }
 
 string GameManager::getMapName() {
-  char MapName[32];
-  if (!mem.read((void*)(mem.map_name_addr), &MapName, sizeof(MapName)))
-    throw runtime_error("Could not get MapName.");
-  // mem.read((void*)(Address + OFFSET_MAPNAME), &MapName, sizeof(MapName));
-  string map_path(MapName);
-  // vector<string> no_path = split_string(map_path, "/");
-  vector<string> no_bsp = split_string(map_path, ".");
-  string ret(no_bsp[0]);
-  return ret;
+  string map_name = "";
+  if (settings.find_map) {
+    if (settings.debug)
+      cout << "Scanning for map..." << endl;
+    if (settings.radar_generic)
+      map_name = "generic";
+    while (map_name == "") {
+      char MapName[32];
+      if (!mem.read((void *)(mem.map_name_addr), &MapName, sizeof(MapName)))
+        throw runtime_error("Could not get MapName.");
+      // mem.read((void*)(Address + OFFSET_MAPNAME), &MapName, sizeof(MapName));
+      string map_path(MapName);
+      // vector<string> no_path = split_string(map_path, "/");
+      vector<string> no_bsp = split_string(map_path, ".");
+      map_name = string(no_bsp[0]);
+      this_thread::sleep_for(chrono::milliseconds(1000));
+    }
+    cout << "Found Map: " << map_name << endl;
+  } else {
+    cout << "Map detection deactivated. Please choose map:" << endl;
+    cin >> map_name;
+  }
+  return map_name;
 }
 
 bool GameManager::isScoped(addr_type player_addr) {
   char buf;
-  if(!mem.read((void*) (player_addr + mem.m_bIsScoped), &buf, sizeof(buf)))
-     return false;
-  return (bool) buf;
+  if (!mem.read((void *)(player_addr + mem.m_bIsScoped), &buf, sizeof(buf)))
+    return false;
+  return (bool)buf;
 }
 
 Team GameManager::getTeam(addr_type player_addr) {
   unsigned int team;
-  if(!mem.read((void*) (player_addr + mem.m_iTeamNum), &team, sizeof(team)))
+  if (!mem.read((void *)(player_addr + mem.m_iTeamNum), &team, sizeof(team)))
     throw runtime_error("Could not get Team.");
-  return (Team) team;
+  return (Team)team;
 }
 
 unsigned int GameManager::getCrosshairTarget(addr_type player_addr) {
   unsigned int target;
-  if(!mem.read((void*) (player_addr + mem.m_iCrosshairIndex), &target, sizeof(target)))
+  if (!mem.read((void *)(player_addr + mem.m_iCrosshairIndex), &target,
+                sizeof(target)))
     throw runtime_error("Could not get CrosshairTarget.");
   return target;
 }
 
 QAngle GameManager::getAimPunch(addr_type player_addr) {
   QAngle ang;
-  if(!mem.read((void*) (player_addr + mem.m_Local + mem.m_aimPunchAngle), &ang, sizeof(ang)))
+  if (!mem.read((void *)(player_addr + mem.m_Local + mem.m_aimPunchAngle), &ang,
+                sizeof(ang)))
     throw runtime_error("Could not get AimPunch.");
   return ang;
 }
 
 bool GameManager::isDefusing(addr_type player_addr) {
   char buf;
-  if(!mem.read((void*) (player_addr + mem.m_bIsDefusing), &buf, sizeof(buf)))
+  if (!mem.read((void *)(player_addr + mem.m_bIsDefusing), &buf, sizeof(buf)))
     return false;
-  return (bool) buf;
+  return (bool)buf;
 }
 
 bool GameManager::isFlashed(addr_type player_addr) {
@@ -271,21 +292,23 @@ bool GameManager::isFlashed(addr_type player_addr) {
 
 float GameManager::getFlashDuration(addr_type player_addr) {
   float buf;
-  if(!mem.read((void*) (player_addr + mem.m_flFlashDuration), &buf, sizeof(buf)))
+  if (!mem.read((void *)(player_addr + mem.m_flFlashDuration), &buf,
+                sizeof(buf)))
     return 0.0;
-  return  buf;
+  return buf;
 }
 
 QAngle GameManager::getNetworkAngles(addr_type player_addr) {
   QAngle ang;
-  if(!mem.read((void*) (player_addr + 0x164), &ang, sizeof(ang)))
+  if (!mem.read((void *)(player_addr + 0x164), &ang, sizeof(ang)))
     throw runtime_error("Could not get NetworkAngles.");
   return ang;
 }
 
 Weapon GameManager::getWeapon(addr_type player_addr) {
   unsigned int activeWeaponID;
-  if(!mem.read((void*) (player_addr + mem.m_hActiveWeapon), &activeWeaponID, sizeof(int)))
+  if (!mem.read((void *)(player_addr + mem.m_hActiveWeapon), &activeWeaponID,
+                sizeof(int)))
     return Weapon::NONE;
   // if(settings.debug) cout << "Active weaponID:" << activeWeaponID << endl;
   activeWeaponID &= 0xFFF;
@@ -296,42 +319,46 @@ Weapon GameManager::getWeapon(addr_type player_addr) {
       continue;
     if (!mem.read(g_glow[i].m_pEntity, &currentEntity, sizeof(EntityType)))
       continue;
-    if (currentEntity.m_iEntityId == activeWeaponID){ // found entity for weapon
+    if (currentEntity.m_iEntityId ==
+        activeWeaponID) { // found entity for weapon
       // if(settings.debug) cout << "Found entity" << endl;
       // get weapon type
-      mem.read((void *)((addr_type)g_glow[i].m_pEntity + mem.m_AttributeManager + 0x60 + mem.m_iItemDefinitionIndex), &weaponID, sizeof(int));
+      mem.read((void *)((addr_type)g_glow[i].m_pEntity +
+                        mem.m_AttributeManager + 0x60 +
+                        mem.m_iItemDefinitionIndex),
+               &weaponID, sizeof(int));
       weaponID &= 0xFFF;
       // if(settings.debug) cout << "weaponID:" << weaponID << endl;
       break;
     }
   }
-  return (Weapon) weaponID;
+  return (Weapon)weaponID;
 }
 
 std::vector<Vector> GameManager::getSmokeLocations() {
   // super hacky way to detect smokes from the entity list:
   // if a non player entity has FL_ONGROUND set it's a smoke
   std::vector<Vector> result;
-  for (EntityType* entity : nonPlayerEntities) {
-    if (!(entity->m_fFlags & FL_CLIENT) && (entity->m_fFlags & FL_ONGROUND) ) {
+  for (EntityType *entity : nonPlayerEntities) {
+    if (!(entity->m_fFlags & FL_CLIENT) && (entity->m_fFlags & FL_ONGROUND)) {
       result.push_back(entity->m_vecOrigin);
     }
   }
   return result;
 }
 
-
-bool GameManager::lineThroughSmoke(Vector start, Vector end){
+bool GameManager::lineThroughSmoke(Vector start, Vector end) {
   std::vector<Vector> smokeLocations = getSmokeLocations();
   Vector d = end - start;
   normalize_vector(&d);
   for (Vector smoke : smokeLocations) {
-    if(lineSphereIntersection(start, end, smoke, smokeRadius))
+    if (lineSphereIntersection(start, end, smoke, smokeRadius))
       return true;
   }
   return false;
 }
 
 bool GameManager::isLocalPlayerFlashed() {
-  return getFlashDuration(mem.local_player_addr) - local_player_flashed_timer > FLASH_END_OFFSET;
+  return getFlashDuration(mem.local_player_addr) - local_player_flashed_timer >
+         FLASH_END_OFFSET;
 };
